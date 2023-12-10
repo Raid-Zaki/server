058f22bd4347973912f4fdbbf4746960b6127ec3
RaidOuahioune <raidrockstar040@gmail.com>
2023-12-10 02:48:27 +0100

    adding seeder + basic schema with orm relations

--- Dockerfile
+++ Dockerfile
@@ -8,14 +8,13 @@ WORKDIR $WORKDIR
 RUN pip install --upgrade pip
 RUN pip install poetry==1.7.1
 
-
 COPY pyproject.toml ./
-
 
 RUN poetry config virtualenvs.create false \
     && poetry install
 
 RUN poetry lock
+
 
 COPY . .
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000", "--reload"]
--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 
+
 from routes.post_router import router as post_router
-from routes.auth_router import router as user_router
+from routes.auth_router import router as auth_router
+from routes.test_router import router as test_router
 from models.core import HealthResponse
 app = FastAPI()
 origins = ["*"]
@@ -16,8 +18,8 @@ app.add_middleware(
 )
 
 app.include_router(router=post_router, prefix="/posts")
-app.include_router(router=user_router, prefix="/auth")
-
+app.include_router(router=auth_router, prefix="/auth")
+app.include_router(router=test_router, prefix="/tests")
 @app.get("/", response_model=HealthResponse)
 async def health():
     return HealthResponse(status="Ok")
