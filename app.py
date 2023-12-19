651b050c2b9d3ab2c9f7585049ebee76bc577503
RaidOuahioune <raidrockstar040@gmail.com>
2023-12-19 11:32:39 +0100

    linking the embeddings with the main db and orm schema

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ models={}
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Load the ML model
-    #models[Embedders.FLAN_SMALL.value]=HuggingFaceBgeEmbeddings(model_name=Embedders.FLAN_SMALL.value,cache_folder=os.getenv("CACHE_FOLDER"))
+    models[Embedders.FLAN_SMALL.value]=HuggingFaceBgeEmbeddings(model_name=Embedders.FLAN_SMALL.value,cache_folder=os.getenv("CACHE_FOLDER"))
     yield
     # Clean up the ML models and release the resources
     models.clear()
