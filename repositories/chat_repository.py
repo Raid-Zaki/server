220f3598521467ddb3db9afdfcefbf938d1aa26c
RaidOuahioune <raidrockstar040@gmail.com>
2024-01-26 20:17:04 +0100

    fix some issues with the prompt

--- repositories/chat_repository.py
+++ repositories/chat_repository.py
@@ -33,20 +33,14 @@ class ChatRepository:
 
     @staticmethod 
     async def __chat_creation_template(query:ChatQuery,id:int,db:Session)->Message:
-        template = """Answer the question based only on the following context:
-        {context}
-        Question: {question}
-        """
         model = ChatRepository.__get_chat_model()
         retriever =await  VectorRepository.query(query.query, id, db)
-
         template = """Answer the question based only on the following context:
         {context}
 
         Question: {question}
         """
         prompt = ChatPromptTemplate.from_template(template)
-
         chain = (
                 {"context": retriever ,"question": RunnablePassthrough()}
                 | prompt
@@ -62,13 +56,14 @@ class ChatRepository:
         history=ChatRepository.__prepare_history(messages)
         retriever =await  VectorRepository.query(query.query, id, db)
         memory = ConversationBufferMemory(memory_key="chat_history", chat_memory=history,return_messages=True,output_key='answer')
-        template = """Given the following conversation respond to the best of your ability in a pirate voice and end every sentence with Ay Ay Matey
+        template = """Given the following conversation history and the context given , do your best to answer the human question:
         Useful Informations:
         {context}
-        Chat History:
-        {chat_history}
-        Follow Up Input: {question}
-        Standalone question:"""
+        
+        Chat History:{chat_history}
+        
+        Current Question: {question}
+        """
         PROMPT = PromptTemplate(
             input_variables=["chat_history", "question"], 
             template=template
